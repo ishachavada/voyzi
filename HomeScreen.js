8660876d864f7819b492edaf749641f5471e4b88
isha <ishchavada@gmail.com>
2025-05-01 23:42:32 +0530

    101

--- HomeScreen.js
+++ HomeScreen.js
@@ -106,7 +106,6 @@ const HomeScreen = () => {
                   onFocus={() => navigation.navigate('Search')} 
                 />
 
-                <MaterialIcons name="tune" size={20} color="black" />
               </View>
 
               {/* Categories */}
